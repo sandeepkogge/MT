acd7b9232c8a5712cda31f4525136e65f398f0ed
sandeepkogge <sandeep.kogge@digitalgroove.in>
2023-03-13 01:38:29 +0530

    Check2
    Check2

---
+++ Check.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  9 08:58:06 2023
+
+@author: Lenovo
+"""
+
+cpu_gpu_option = "GPU"
+cpu_gpu_number = 5
+cpu_gpu = cpu_gpu_option + ":" + str(cpu_gpu_number)
